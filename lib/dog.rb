4c42616199359f20cf0ca7cff2ced2a7116294f4
Alison Reilly <reillyamr@gmail.com>
2019-03-06 14:14:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,22 +10,7 @@ class Dog < ActiveRecord::Base
     
   end
   
-  def self.create(name:, breed:)
-    dog = Dog.new(name: name, breed: breed)
-    dog.save
-    dog
-  end
-  
-  def save
-    sql = "INSERT INTO dogs (name, breed) VALUES = (?, ?)"
-   
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
-  end 
-  
-  def update
-    sql = "UPDATE dogs SET name = ?, breed = ?  WHERE id = ?"
-    DB[:conn].execute(sql, self.name, self.breed, self.id)
-  end
+    Dog.create(name:)
   
    
   
