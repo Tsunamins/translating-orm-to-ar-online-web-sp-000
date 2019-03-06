c683880b3cd6210a014b5166f49a550eed0afb02
Alison Reilly <reillyamr@gmail.com>
2019-03-06 14:07:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,34 @@
-class Dog
+
+
+class Dog < ActiveRecord::Base
+  attr_accessor :name, :breed
+  
+   def initialize(name:, breed:)
+    @name = name 
+    @breed = breed 
+    
+    
+  end
+  
+  def self.create(name:, breed:)
+    dog = Dog.new(name: name, breed: breed)
+    dog.save
+    dog
+  end
+  
+  def save
+    sql = "INSERT INTO dogs (name, breed) VALUES = (?, ?)"
+   
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+  end 
+  
+  def update
+    sql = "UPDATE dogs SET name = ?, breed = ?  WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
+  
+   
+  
+  
+  
 end
